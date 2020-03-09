73bfd705385400fa2bd029d937821a926c9c217c
ebisLab <ebaiden@techie.com>
2020-03-09 16:23:08 -0400

    assigned port

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express();
+
+server.use(express.json());
+
+
+const PORT = 6000;
+server.listen(PORT, ()=>{
+    console.log(`\n ** API on http://localhost:${PORT} **\n`)
+});
